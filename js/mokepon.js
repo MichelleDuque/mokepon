1cbdc3fb4295ecbf225447f30b5d5b06ab0d7b71
MichelleDuque <michelled_1994@hotmail.com>
2022-07-23 00:44:25 -0500

    Se agregaron mensajes de ataques

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,7 +1,17 @@
+let ataqueJugador 
+let ataqueEnemigo
+
+
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById("boton-mascota")
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
-
+    
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener("click", ataqueFuego)
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.addEventListener("click", ataqueAgua)
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.addEventListener("click", ataqueTierra)
 }
 
 function seleccionarMascotaJugador(){
@@ -27,18 +37,58 @@ function seleccionarMascotaJugador(){
 }
 
 function seleccionarMascotaEnemigo(){
-    let ataqueAleatorio = aleatorio(1,3)
+    let mascotaAleatorio = aleatorio(1,3)
     let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
 
-    if (ataqueAleatorio == 1){
+    if (mascotaAleatorio == 1){
         spanMascotaEnemigo.innerHTML = "Hipodoge"
     }
-    else if (ataqueAleatorio == 2){
+    else if (mascotaAleatorio == 2){
         spanMascotaEnemigo.innerHTML = "Capipepo"
     }
-    else if (ataqueAleatorio == 3){
+    else if (mascotaAleatorio == 3){
         spanMascotaEnemigo.innerHTML = "Ratigueya"
     } 
+}
+
+function ataqueFuego(){
+    ataqueJugador = "FUEGO"
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAgua(){
+    ataqueJugador = "AGUA"
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueTierra(){
+    ataqueJugador = "TIERRA"
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    // let spanAtaqueEnemigo = document.getElementById("mascota-enemigo")
+
+    if (ataqueAleatorio == 1){
+        ataqueEnemigo = "FUEGO"
+    }
+    else if (ataqueAleatorio == 2){
+        ataqueEnemigo = "AGUA"
+    }
+    else if (ataqueAleatorio == 3){
+        ataqueEnemigo = "TIERRA"
+    }
+
+    crearMensaje() 
+}
+
+function crearMensaje(){
+    let sectionMensajes = document.getElementById("mensajes")
+
+    let parrafo = document.createElement("p")
+    parrafo.innerHTML = "Tu mascota atacó con " + ataqueJugador + ", la mascota del enemigo atacó con " + ataqueEnemigo + " - PENDIENTE"
+    sectionMensajes.appendChild(parrafo)
 }
 
 function aleatorio(min,max){
