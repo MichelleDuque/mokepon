8f819f88ccd51e52530ca76b3074d685ba37e253
MichelleDuque <michelled_1994@hotmail.com>
2022-07-27 00:13:15 -0500

    Se hizo cambios de estilos en resultado final

--- js/mokepon.js
+++ js/mokepon.js
@@ -30,7 +30,7 @@ function seleccionarMascotaJugador(){
     let sectionSeleccionarMascota = document.getElementById("seleccionar-mascota")
     sectionSeleccionarMascota.style.display = "none"
     let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
-    sectionSeleccionarAtaque.style.display = "block"
+    sectionSeleccionarAtaque.style.display = "flex"
 
     let inputHipodoge = document.getElementById("hipodoge")
     let inputCapipepo = document.getElementById("capipepo")
@@ -130,27 +130,33 @@ function combate(){
 
 function revisarVidas(){
     if (vidasEnemigo == 0){
-        crearMensajeFinal("Felicitaciones! Ganaste😀")
+        crearMensajeFinal("¡Felicitaciones! 🎉Ganaste🎉")
     } else if (vidasJugador == 0){
-        crearMensajeFinal("Lo siento, perdiste 😢")
+        crearMensajeFinal("Lo siento, Perdiste 😢😖")
     }
 }
 
 
 function crearMensaje(resultado){
-    let sectionMensajes = document.getElementById("mensajes")
+    let sectionMensajes = document.getElementById("resultado")
+    let ataquesDelJugador= document.getElementById("ataques-del-jugador")
+    let ataquesDelEnemigo = document.getElementById("ataques-del-enemigo")
 
-    let parrafo = document.createElement("p")
-    parrafo.innerHTML = "Tu mascota atacó con " + ataqueJugador + ", la mascota del enemigo atacó con " + ataqueEnemigo + " - " + resultado
-    sectionMensajes.appendChild(parrafo)
+    let nuevoAtaqueDelJugador = document.createElement("p")
+    let nuevoAtaqueDelEnemigo = document.createElement("p")
+
+    sectionMensajes.innerHTML = resultado
+    nuevoAtaqueDelJugador.innerHTML = ataqueJugador
+    nuevoAtaqueDelEnemigo.innerHTML = ataqueEnemigo
+ 
+    ataquesDelJugador.appendChild(nuevoAtaqueDelJugador)
+    ataquesDelEnemigo.appendChild(nuevoAtaqueDelEnemigo)
 }
 
 function crearMensajeFinal(resultadoFinal){
-    let sectionMensajes = document.getElementById("mensajes")
+    let sectionMensajes = document.getElementById("resultado")
 
-    let parrafo = document.createElement("p")
-    parrafo.innerHTML = resultadoFinal
-    sectionMensajes.appendChild(parrafo)
+    sectionMensajes.innerHTML = resultadoFinal
 
     let botonFuego = document.getElementById("boton-fuego")
     botonFuego.disabled = true
